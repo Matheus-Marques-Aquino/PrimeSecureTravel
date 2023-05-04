961d1084103d116d810812d1838a0c69bc410cd7
Matheus Marques <matheus.marques.aquino@gmail.com>
2023-05-04 18:00:23 -0300

    0.1

---
+++ collections/tokens.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const tokenSchema = new mongoose.Schema({
+    access_token: { type: String },
+    token_type: { type: String },
+    refresh_token: { type: String },
+    expires_in: { type: String },
+    created_at: { type: Date }
+});
+
+const Tokens = mongoose.model('Tokens', tokenSchema);
+
+module.exports = Tokens;
